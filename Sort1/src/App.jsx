1b5126b20bbef030e3793b937d3a02b46be7ed98
Daksh584 <22je0292@iitism.ac.in>
2024-06-25 15:58:16 +0530

    first commit

---
+++ Sort1/src/App.jsx
@@ -0,0 +1,11 @@
+// src/App.js
+import SortingVisualizer from './SortingVisualizer/SortingVisualizer.jsx';
+ 
+const App = () => {
+
+  return (
+    <SortingVisualizer></SortingVisualizer>
+  );
+};
+
+export default App;
